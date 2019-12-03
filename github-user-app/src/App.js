317f65f8f154c7537e99341087f81367fd9bcd83
Alison Law <alisparklaw@gmail.com>
2019-12-03 12:21:41 +0000

    Create-React-App and file structure set up

---
+++ github-user-app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">GitHub User Card App</div>;
+}
+
+export default App;
