0b4fa208e2800abe3e9a7f22401f7c728e081a38
Alison Law <alisparklaw@gmail.com>
2019-12-03 14:03:03 +0000

    axios request recieving data

--- github-user-app/src/App.js
+++ github-user-app/src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import GithubUserData from "./Components/GithubUserData";
+
 function App() {
-  return <div className="App">GitHub User Card App</div>;
+  return (
+    <div>
+      <div className="App">GitHub User Card App</div>;
+      <GithubUserData />
+    </div>
+  );
 }
 
 export default App;
---
+++ github-user-app/src/Components/GithubUserData.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function GithubUserData() {
+  const [users, fetchUsers] = useState([]);
+
+  useEffect(() => {
+    console.log("GithubUserData component mounted");
+    axios
+      .get("https://api.github.com/users/Aliszl/followers")
+      .then(response => {
+        console.log(response);
+        console.log(response.data);
+
+        fetchUsers(response.data);
+      })
+      .catch(error => {
+        console.log("the data was not returned", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => {
+        return <div>{user.name}</div>;
+      })}
+    </div>
+  );
+}
+
+// export default class GithubUserData extends React.Component {
+//   state = {
+//     users: []
+//   };
+
+//   componentDidMount() {
+//     axios
+//       .get("https://api.github.com/users/Aliszl/followers")
+//       .then(response => {
+//         console.log(response);
+//         console.log(response.data);
+//         this.setState({ users: response.data });
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.users.map(user => (
+//           <li key={user.name}>{user.name}</li>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+// <ul>
+//   {this.state.users.map(user => (
+//     <li key={user.name}>{user.name}</li>
+//   ))}
+// </ul>;
